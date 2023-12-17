1f7055aee9354ccfcae5f51eb05cbd2270f2356b
Jeremie Feron <101343852+eBoreal@users.noreply.github.com>
2023-12-17 18:21:21 +0100

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,17 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backgroundColor: {
+        'primary': '#007bff', // Example: a blue primary color
+        'primary-foreground': `rgb(var(--foreground-rgb))`
+      },
+      textColor: {
+        'primary-foreground': `#FFFFFF`,
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
